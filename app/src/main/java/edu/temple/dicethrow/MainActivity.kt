57498f09914497790bd821b2e5cf11e03a093f6d
Riviera Sperduto <riviera.t.sperduto13@gmail.com>
2025-03-26 15:37:09 -0400

    finished the lab??

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -1,10 +1,11 @@
 package edu.temple.dicethrow
 
+import android.content.res.Configuration
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 
 
 /*
@@ -19,24 +20,41 @@ The Activity layout files for both Portrait and Landscape are already provided
 */
 
 class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
+
+    private var isLandscape = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /* TODO 1: Load fragment(s)
-            - Show _only_ ButtonFragment if portrait
-            - show _both_ fragments if Landscape
-          */
+        isLandscape = resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+
+        // TODO 1: Load fragment(s)
+        // - Show _only_ ButtonFragment if portrait
+        // - show _both_ fragments if Landscape
+        if (savedInstanceState == null) {
+            if (isLandscape) {
+                supportFragmentManager.commit {
+                    add<ButtonFragment>(R.id.container1)
+                    add<DieFragment>(R.id.container2)
+                }
+            } else {
+                supportFragmentManager.commit {
+                    add<ButtonFragment>(R.id.container1)
+                }
+            }
+        }
     }
 
-    /* TODO 2: switch fragments if die rolled and in portrait (no need to switch fragments if Landscape)
-        */
-
+    // TODO 2: switch fragments if die rolled and in portrait (no need to switch fragments if Landscape)
     // This callback function gets invoked when the child Fragment invokes it
     // Remember to place Fragment transactions on BackStack so then can be reversed
     override fun buttonClicked() {
-
+        if (!isLandscape) {
+            supportFragmentManager.commit {
+                replace<DieFragment>(R.id.container1)
+                addToBackStack(null)
+            }
+        }
     }
-
-
 }
